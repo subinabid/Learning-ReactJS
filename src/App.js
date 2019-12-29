070fb328a4fd68d6336896c1262981306e0f55a9
Subin Abid <subinabid@gmail.com>
2019-12-29 23:01:17 +0530

    Understanding Basics
    elements, props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table';
 
-function App() {
+function App0() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>First App Trial</h1>
     </div>
   );
+}
+
+class App extends Component {
+  render() {
+    
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+
+    return (
+      <div className = "container">
+        <Table characterData = {characters} />
+      </div>
+    )
+  }
 }
 
 export default App;
