ccaf1be0fed403d7393f3af3b4f4c8465c9b69f2
Subin Abid <subinabid@gmail.com>
2019-12-31 17:23:15 +0530

    Apis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+//import App from './App'
+import App from './Api'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
